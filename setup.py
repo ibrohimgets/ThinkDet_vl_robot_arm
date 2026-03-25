d84f3c0a03575d9aa9dad21bc07a9a92503cbfa7
Ibrohim Muminov <ibrohimuminov@gmail.com>
2026-03-25 09:07:50 +0000

    Fix ROS package setup entrypoint

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from glob import glob
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 package_name = "thinkdet_ros2_demo"
@@ -8,10 +8,10 @@ package_name = "thinkdet_ros2_demo"
 setup(
     name=package_name,
     version="0.1.0",
-    packages=find_packages(include=["ROS", "ROS.*"]),
+    py_modules=["thinkdet_ros2_node", "thinkdet_runtime"],
     data_files=[
         ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
-        (f"share/{package_name}", ["package.xml"]),
+        (f"share/{package_name}", ["package.xml", "README.md", "LICENSE"]),
         (f"share/{package_name}/launch", glob("launch/*.py")),
     ],
     install_requires=["setuptools"],
@@ -22,7 +22,7 @@ setup(
     license="MIT",
     entry_points={
         "console_scripts": [
-            "thinkdet_grasp_node = ROS.thinkdet_ros2_node:main",
+            "thinkdet_grasp_node = thinkdet_ros2_node:main",
         ],
     },
 )
